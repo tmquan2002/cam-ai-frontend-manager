a7cf89b9cd82d628259f37afcbc5667d1a5ef44e
tmquan <quantm.lqd@gmail.com>
2023-12-19 13:12:07 +0700

    Add fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Button, Group } from '@mantine/core'
+import './App.css'
 
 function App() {
 
@@ -6,6 +7,7 @@ function App() {
     <Group>
       <Button>Primary button</Button>
       <Button color="light-yellow.9">Yellow button</Button>
+      <div className='viet-class'>Testing fonts Việt</div>
     </Group>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import '@mantine/core/styles.css';
+import './index.css';
 import { MantineProvider, createTheme } from '@mantine/core';
 import { light_blue, light_yellow, pale_red, shading } from './constants/Color.ts';
+import './assets/fonts/DesignerVN-Poppins-Regular.ttf'
 
 //TODO: Setup main theme here with different colors
 const theme = createTheme({
+  fontFamily: 'DVN-Poppins',
   primaryColor: 'light-blue',
   colors: {
     'light-blue': light_blue, 
