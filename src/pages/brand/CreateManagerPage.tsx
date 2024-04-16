4b22d44664091d1fe81b1cbca878f7707a7587ec
Duy <nguyenduduy233@gmail.com>
2024-04-16 22:51:32 +0700

    More sutff

--- src/pages/brand/CreateManagerPage.tsx
+++ src/pages/brand/CreateManagerPage.tsx
@@ -36,7 +36,7 @@ export type CreateAccountField = {
 const CreateManagerPage = () => {
   const navigate = useNavigate();
   const { data: brandList, isLoading: isGetBrandListLoading } = useGetBrandList(
-    { size: 1 }
+    { size: 1 },
   );
 
   const { mutate: createAccount, isLoading: isCreateAccountLoading } =
@@ -46,14 +46,16 @@ const CreateManagerPage = () => {
     validate: {
       name: isNotEmpty("Name is required"),
       email: (value) =>
-        /^\S+@(\S+\.)+\S{2,4}$/g.test(value) ? null : "An email should have a name, @ sign, a server name and domain in order and no whitespace. Valid example abc@email.com",
+        /^\S+@(\S+\.)+\S{2,4}$/g.test(value)
+          ? null
+          : "An email should have a name, @ sign, a server name and domain in order and no whitespace. Valid example abc@email.com",
       password: (value) => {
         if (value == "") {
-            return "Password must not be empty";
+          return "Password must not be empty";
         }
         let confPass = createAccountForm.values.confirmPassword;
         if (confPass != "" && confPass != value) {
-            return "Password and confirm password are not the same"
+          return "Password and confirm password are not the same";
         }
         return null;
       },
@@ -61,8 +63,8 @@ const CreateManagerPage = () => {
       gender: isNotEmpty("Please select gender"),
       phone: (value) =>
         value == "" ||
-          value == null ||
-          /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/g.test(value)
+        value == null ||
+        /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/g.test(value)
           ? null
           : "A phone number should have a length of 10-12 characters",
     },
@@ -226,18 +228,9 @@ const CreateManagerPage = () => {
     isCreateAccountWardsLoading,
   ]);
   return (
-    <Paper
-      m={rem(32)}
-      p={rem(32)}
-    >
-    <BackButton />
-      <Text
-        size="lg"
-        fw={"bold"}
-        fz={25}
-        c={"light-blue.4"}
-        mb={rem(30)}
-      >
+    <Paper m={rem(32)} p={rem(32)}>
+      <BackButton />
+      <Text size="lg" fw={"bold"} fz={25} c={"light-blue.4"} mb={rem(30)}>
         Create shop manager
       </Text>
       <form
@@ -284,14 +277,11 @@ const CreateManagerPage = () => {
                 });
               },
             });
-          }
+          },
         )}
       >
         <EditAndUpdateForm fields={addNewAccountfields} />
-        <Group
-          justify="flex-end"
-          mt="md"
-        >
+        <Group justify="flex-end" mt="md">
           <Button
             type="submit"
             loading={isCreateAccountLoading || isGetBrandListLoading}
